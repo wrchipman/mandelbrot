806162ad657c97ab36b897edc9b85f7a6f76cda7
William Chipman <wrchipman@gmail.com>
2022-01-17 22:35:00 -0300

    starter functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use num::Complex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn square_loop(mut x: f64) {
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64) {
+    let mut x = 0.;
+    loop {
+        x = x * x + c;
+    }
+}
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re:0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex {re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
 }
